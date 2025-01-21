c781efa1e4efe13a38bf42e38e7cb90d38633f43
mounikaboggavarapu <mounikaboggavarapu123@gmail.com>
2025-01-20 23:46:51 -0500

    commit

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+function toggleMore(id) {
+    const moreText = document.getElementById(`${id}-more`);
+    const button = document.querySelector(`#${id} .show-more-btn`);
+
+    if (moreText.style.display === "inline") {
+      moreText.style.display = "none";
+      button.innerText = "Show More";
+    } else {
+      moreText.style.display = "inline";
+      button.innerText = "Show Less";
+    }
+  }
